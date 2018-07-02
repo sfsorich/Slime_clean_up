bf72b75189f6f6d76a798b91e00e47dfbc30138a
dallakhv <dallakhv@ucsc.edu>
2018-07-02 16:25:48 -0700

    Added a simple 3d scene, just trying to see what would look better. Textures are just from google images we would need to make our own.

---
+++ Slime Clean Up/Assets/PlayerController3D.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3D : MonoBehaviour {
+    public float speed = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.GetComponent<Rigidbody>().velocity += Vector3.fwd * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.localScale = new Vector3(-1, 1, 1);
+            this.GetComponent<Rigidbody>().velocity += Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.GetComponent<Rigidbody>().velocity += Vector3.back * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+            this.GetComponent<Rigidbody>().velocity += Vector3.right * speed;
+        }
+    }
+}
