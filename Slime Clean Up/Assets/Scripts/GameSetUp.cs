f24d3093da8519b85f6370ebd7f4aaa136bae002
Benjamin Hallsted <bhallste@ucsc.edu>
2018-07-22 23:07:34 -0700

    git stuff for menus working hopefully

---
+++ Slime Clean Up/Assets/Scripts/GameSetUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetUp : MonoBehaviour {
+
+    GameObject[] SlimeObjects;
+
+    private GameObject SlimeSetUp;
+    private int ActiveSlimes;
+
+    // Use this for initialization
+    void Start () {
+        SlimeObjects = GameObject.FindGameObjectsWithTag("Slime");
+        SlimeSetUp = GameObject.Find("InfoStorage");
+        ActiveSlimes = SlimeSetUp.GetComponent<InfoTransfer>().slimeNum;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public int GetNum()
+    {
+        return ActiveSlimes;
+    }
+    void SettingActiveSlimes()
+    {
+        if (ActiveSlimes == 1)
+        {
+            SlimeObjects[1].SetActive(false);
+            SlimeObjects[2].SetActive(false);
+            Debug.Log("1 slime active");
+        }
+        else if(ActiveSlimes == 2)
+        {
+            SlimeObjects[2].SetActive(false);
+            Debug.Log("2 slimes active");
+        }
+        else
+        {
+            Debug.Log("error active slimes not set");
+        }
+    }
+}
--- Slime Clean Up/Assets/Scripts/UIManegerMenu.cs
+++ Slime Clean Up/Assets/Scripts/UIManegerMenu.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class UIManegerMenu : MonoBehaviour
 {
     //this exists just to make sure time scale is on when heading back to menu
+    private GameObject SlimeCounter;
 
     // Use this for initialization
     void Start()
     {
         Time.timeScale = 1;
+        SlimeCounter = GameObject.Find("InfoStorage");
     }
 
     // Update is called once per frame
@@ -19,4 +22,9 @@ public class UIManegerMenu : MonoBehaviour
 
     }
     //to load or restart a scene use LoadSceneOnClick instead
+    public void SlimeSet(int SlimeCount)
+    {
+        SlimeCounter.GetComponent<InfoTransfer>().slimeNum = SlimeCount;
+        DontDestroyOnLoad(SlimeCounter);
+    }
 }
